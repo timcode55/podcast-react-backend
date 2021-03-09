a1b0030f2b3d5d468d94830d32a9915ba842005f
Tim Petrvalsky <tpet55@yahoo.com>
2021-03-09 11:33:52 -0800

    Fix cors

--- db/Rating.js
+++ db/Rating.js
@@ -3,14 +3,15 @@ const mongoose = require('mongoose');
 const Rating = mongoose.model(
 	'Rating',
 	mongoose.Schema({
-		title: { type: String, index: true },
+		title: { type: String, index: true, unique: true, dropDups: true },
+		id: String,
 		rating: Number,
+		image: String,
 		numberOfRatings: Number,
 		genre: String,
 		description: String,
 		website: String,
 		itunes: String,
-		categoryid: Number,
 		itunesid: Number,
 		listennotesurl: String
 	})
